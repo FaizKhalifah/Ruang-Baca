2624716a22e458deebe88cd16cdb89fcc8297833
Faiz Khalifah <faiz_doc@outlook.com>
2024-09-22 22:31:56 +0700

    feat : admin layout

--- client/src/views/admin/dashboard.vue
+++ client/src/views/admin/dashboard.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>Ini halaman dashboard</h1>
+    <adminLayout>
+        <h1>Ini halaman dashboard</h1>
+    </adminLayout>
 </template>
 
 <script>
+    import adminLayout from '@/components/admin/adminLayout.vue';
     export default{
-        name:'dashboardView'
+        name:'dashboardView',
+        components:{
+            adminLayout
+        }
     }
+
 </script>
