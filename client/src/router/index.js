273c4c6397128e0708d6d886646c3e8701012045
Faiz Khalifah <faiz_doc@outlook.com>
2024-10-11 10:25:07 +0700

    feat: route basic protection

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,13 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import dashboard from '@/views/admin/dashboard.vue'
 import BooksView from '@/views/admin/booksView.vue'
-
+import login from '@/views/auth/login.vue'
+import register from '@/views/auth/register.vue'
 
 const routes = [
   {
-    path: '/',
+    path:'/login',
+    name:'login',
+    component:login
+  },
+  {
+    path:'/register',
+    name:'register',
+    component:register
+  },
+  {
+    path: '/admin/dashboard',
     name: 'home',
-    component: dashboard
+    component: dashboard,
+    meta: { requiresAuth: true}
   },
   {
     path: '/admin/books',
@@ -20,5 +32,22 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  const userRole = localStorage.getItem('role');
+
+  if (to.meta.requiresAuth) {
+    if (!token) {
+      next('/login');
+    } else if (to.meta.role && to.meta.role !== userRole) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
